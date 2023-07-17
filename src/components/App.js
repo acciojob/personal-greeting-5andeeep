4231437c13744c56e24540c64b6e98843a23c29f
Sandeep <newsandeep03@gmail.com>
2023-07-17 21:43:37 +0530

    ans added first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [greet, setGreet] = useState("");
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <label >Enter your name:</label><br />
+        <input onChange={(event) => setGreet("Hello "+event.target.value+"!")} type="text" />
+        <p>{greet}</p>
     </div>
   )
 }
